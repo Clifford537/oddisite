fe5513f189a4569dd575b3235269290c74e695ca
Clifford537 <cliffordmukosh@gmail.com>
2024-07-06 11:25:27 +0300

    updates

--- layout/header.php
+++ layout/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Colloh Best Free Odds</title>
+    <meta name="description" content="Colloh Best Free Odds offers league predictions, articles, jackpots, and more. Join us for reliable betting insights.">
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
@@ -81,9 +82,3 @@
         </div>
     </nav>
 
-    <!-- Bootstrap JS, Popper.js, and jQuery -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
